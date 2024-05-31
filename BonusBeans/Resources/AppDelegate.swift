ee9f478aa3926321476c08332f340d1072907c1b
Khasan Davronbekov <xasand3103@gmail.com>
2024-05-31 09:35:12 +0500

    added executable files

---
+++ BonusBeans/Resources/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  BonusBeans
+//
+//  Created by Хасан Давронбеков on 31/05/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
